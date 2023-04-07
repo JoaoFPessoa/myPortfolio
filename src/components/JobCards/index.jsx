ef6ad830dd2a283443fac4d2121a52f28701aa02
JoaoFPessoa <joaoppessoa@uol.com.br>
2023-04-07 14:20:12 -0300

    removed unused files

--- src/components/JobCards/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export default function JobCard({ link, title, image, button }) {
-  return (
-    <Container href={link} target="_blank" image={image}>
-      <h1>{title}</h1>
-    </Container>
-  );
-}
