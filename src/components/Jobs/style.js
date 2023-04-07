eaf7a8a66cb910e0ee864bf5e27b9964db6cbb6d
JoaoFPessoa <joaoppessoa@uol.com.br>
2023-04-07 13:55:45 -0300

    add/ jobs component

--- src/components/Jobs/style.js
+++ src/components/Jobs/style.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  border: 2px solid green;
   height: 800px;
-  margin-top: 250px;
+  margin-top: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    margin-bottom: 5rem;
+  }
+`;
+export const CardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1rem;
+  margin: 0 10rem;
 `;
